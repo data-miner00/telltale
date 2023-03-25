f67760808e05e34999c60b0635e6463449077ad0
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-25 08:57:45 +0800

    wip: Added simple navigator

--- apps/chat/src/App.vue
+++ apps/chat/src/App.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
 import { RouterView } from 'vue-router';
+import Navigator from './components/Navigator.vue';
 </script>
 
 <template>
   <router-view></router-view>
+  <Navigator />
 </template>
---
+++ apps/chat/src/components/Navigator.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+
+<template>
+  <nav class="fixed right-20 bottom-20">
+    <router-link to="/">Home</router-link>
+    <router-link to="/room/sm">Room</router-link>
+  </nav>
+</template>
