30dae0819cbe27c4019fcd6845dde3d8b93fffbf
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-30 22:34:33 +0800

    feat: Enhance style for message content

--- apps/chat/src/lib/modules/chat/ChatMessage.vue
+++ apps/chat/src/lib/modules/chat/ChatMessage.vue
@@ -17,13 +17,13 @@ defineProps({
 <template>
   <div class="odd:bg-gray-100">
     <div
-      class="w-full mx-auto flex py-6 max-w-md lg:max-w-2xl xl:max-w-4xl gap-4 md:gap-6"
+      class="w-full mx-auto flex py-6 max-w-md lg:max-w-2xl xl:max-w-3xl gap-4 md:gap-6"
     >
       <div class="shrink-0">
         <img class="h-8 w-8 rounded-sm" :src="avatarUrl" alt="User avatar" />
       </div>
       <article
-        class="break-words overflow-hidden"
+        class="break-words overflow-hidden prose w-full max-w-max"
         v-html="marked.parse(message)"
       ></article>
     </div>
