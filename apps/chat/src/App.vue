5d56755aa891c91761476996fa283699edc84683
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-30 08:27:23 +0800

    chore: Retire navigator and roomInfo

--- apps/chat/src/App.vue
+++ apps/chat/src/App.vue
@@ -1,7 +1,6 @@
 <script lang="ts" setup>
 import { onMounted } from 'vue';
 import { RouterView } from 'vue-router';
-import Navigator from './components/Navigator.vue';
 import { socket } from './lib/shared/shared.socket';
 import { useUserStore } from './lib/shared/shared.stores';
 import PageContainer from './lib/modules/page-container/PageContainer.vue';
@@ -18,6 +17,5 @@ socket.on('connect', () => setUserId(socket.id));
 <template>
   <PageContainer>
     <router-view></router-view>
-    <Navigator />
   </PageContainer>
 </template>
