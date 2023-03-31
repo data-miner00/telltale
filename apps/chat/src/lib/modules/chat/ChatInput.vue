0b89293f18424d5c5a94f26711a2a855818a45da
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-31 08:21:12 +0800

    feat: Use textarea instead of input

--- apps/chat/src/lib/modules/chat/ChatInput.vue
+++ apps/chat/src/lib/modules/chat/ChatInput.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 import { Send } from '@/lib/shared/icons';
 
 const message = ref<string>();
+const textareaRef = ref<HTMLTextAreaElement>();
 
 const emit = defineEmits({
   messageFired: null,
@@ -22,10 +23,12 @@ function tryPublishMessage() {
     class="form-control stretch mx-2 gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-3xl"
   >
     <div class="input-group">
-      <input
+      <textarea
         v-model="message"
+        ref="textareaRef"
         type="text"
-        class="input input-bordered flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]"
+        class="input input-bordered resize-none overflow-y-auto w-full py-2 md:py-3 md:pl-4 border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]"
+        @keydown.enter.exact.prevent="() => textareaRef?.form?.requestSubmit()"
       />
       <button type="submit" class="btn btn-square">
         <Send />
@@ -33,3 +36,18 @@ function tryPublishMessage() {
     </div>
   </form>
 </template>
+
+<style scoped lang="scss">
+.input-group textarea {
+  // Firefox
+  scrollbar-width: none;
+
+  // IE, Edge
+  -ms-overflow-style: none;
+}
+
+// Chrome
+.input-group textarea::-webkit-scrollbar {
+  display: none;
+}
+</style>
