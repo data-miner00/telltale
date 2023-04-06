b2029fee49a9997377878bd8f440456636dd9cca
data-miner00 <data-miner00@users.noreply.github.com>
2023-04-06 20:25:37 +0800

    feat: Added simple 404 page

---
+++ apps/chat/src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="h-screen flex justify-center items-center">
+    <div class="m-auto text-center">
+      <h1 class="text-4xl font-bold uppercase">404 Not Found</h1>
+      <p class="mb-2">The page that you are trying to reach is unavailable.</p>
+      <a @click="$router.go(-1)" class="btn btn-primary btn-sm">Take me back</a>
+    </div>
+  </div>
+</template>
