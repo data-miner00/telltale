5e4e80225fc0d629ad65021aa93d8b9167abd6f3
data-miner00 <data-miner00@users.noreply.github.com>
2023-04-07 18:37:30 +0800

    feat: Added simple page navigator

---
+++ apps/labs/src/lib/modules/navigator/navigator.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export const count = 0;
+</script>
+
+<nav class="absolute right-5 top-5 z-20">
+  <ul class="flex items-center justify-center gap-1">
+    <li class="block">
+      <a href="/">Home</a>
+    </li>
+    <li class="block">
+      <a href="/mobile">Mobile</a>
+    </li>
+  </ul>
+</nav>
+
+<style lang="sass">
+ul li a
+    @apply px-2 py-1 cursor-pointer bg-gray-100 border border-solid border-gray-200 rounded
+</style>
--- apps/labs/src/routes/+layout.svelte
+++ apps/labs/src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+  import Navigator from '../lib/modules/navigator/navigator.svelte';
   import '../app.scss';
 </script>
 
 <svelte:head>
   <title>A laboratory to figure out chat layouts</title>
+  <meta name="description" content="Chat Layout Sandbox App" />
 </svelte:head>
+
+<Navigator />
 
 <slot />
