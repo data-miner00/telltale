892196ad9c62050685ad19c7d6b33ed7867651a1
data-miner00 <data-miner00@users.noreply.github.com>
2023-04-15 18:52:05 +0800

    feat: Added default for truncate

--- apps/chat/src/lib/shared/shared.utils.ts
+++ apps/chat/src/lib/shared/shared.utils.ts
@@ -1,4 +1,7 @@
-export function truncateWithEllipsis(text: string, preserve: number = 4) {
+export function truncateWithEllipsis(
+  text: string = '0x0000000000000000',
+  preserve: number = 4
+) {
   return (
     text.substring(0, preserve) + '...' + text.substring(text.length - preserve)
   );
