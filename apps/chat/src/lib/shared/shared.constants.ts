4464425f462a3b6cc4ccd16fb5e658b23fcac468
data-miner00 <data-miner00@users.noreply.github.com>
2023-04-15 18:52:27 +0800

    chore: Added Localstorage prefix

--- apps/chat/src/lib/shared/shared.constants.ts
+++ apps/chat/src/lib/shared/shared.constants.ts
@@ -3,3 +3,4 @@ export const MOCK_AVATAR_URL_FEMALE =
   'https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg';
 export const MOCK_AVATAR_URL_MALE =
   'https://randomuser.me/api/portraits/men/24.jpg';
+export const LOCALSTORAGE_PREFIX = 'telltale-';
