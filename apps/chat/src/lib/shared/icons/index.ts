96c95ea47317343ac22c808759533ca8a5ea3102
data-miner00 <data-miner00@users.noreply.github.com>
2023-05-02 22:20:04 +0800

    feat: Added clipboard and green check icon

--- apps/chat/src/lib/shared/icons/index.ts
+++ apps/chat/src/lib/shared/icons/index.ts
@@ -2,3 +2,5 @@ export { default as ChevronDown } from './ChevronDown.vue';
 export { default as Person } from './Person.vue';
 export { default as Exit } from './Exit.vue';
 export { default as Send } from './Send.vue';
+export { default as Clipboard } from './Clipboard.vue';
+export { default as GreenCheck } from './GreenCheck.vue';
